fa5000ce2cab4b2bca2a3d074d962869a6035e86
BathulaSrinuvas <145068922+BathulaSrinuvas@users.noreply.github.com>
2024-03-06 14:40:28 +0300

    Update Shuffle the Array.py

--- Shuffle the Array.py
+++ Shuffle the Array.py
@@ -7,10 +7,5 @@ class SolutionShuffleArray():
             output.append(x[i])
             output.append(y[i])
         return output
-        # for index, i in enumerate(nums):
-
-        #     for j in range(i+1,nums):
-        #         return i,j
-
 nums = [2,5,1,3,4,7] 
 n = 3
